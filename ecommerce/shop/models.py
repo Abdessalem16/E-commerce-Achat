e35f9e4e3233f9b1a8bee5118fc4dad3c987fc60
abdessalem16 <abdessalemgitlahouel@gmail.com>
2026-08-24 18:35:01 +0200

    partie visuel de l'admin panel

--- ecommerce/shop/models.py
+++ ecommerce/shop/models.py
@@ -1,21 +1,31 @@
 from django.db import models
-from django.db.models import ForeignKey
 
-# Create your models here.
+
 class Category(models.Model):
-    name= models.CharField(max_length=200)
+    name = models.CharField(max_length=200)
     date_added = models.DateTimeField(auto_now=True)
 
     class Meta:
         ordering = ['-date_added']
 
-class Product (models.Model):
-    title =models.CharField(max_length=200)
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    title = models.CharField(max_length=200)
     price = models.FloatField()
     description = models.TextField()
-    Category= ForeignKey(Category,related_name='categorie', on_delete=models.CASCADE)
+    category = models.ForeignKey(
+        Category,
+        related_name='categorie',
+        on_delete=models.CASCADE
+    )
     image = models.CharField(max_length=5000)
-    date_added =models.DateTimeField(auto_now=True)
+    date_added = models.DateTimeField(auto_now=True)
 
     class Meta:
-            ordering = ['-date_added']
+        ordering = ['-date_added']
+
+    def __str__(self):
+        return self.title
