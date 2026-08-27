d77ad41b82db2fcf98bb6e5b1e6fdde73f8acab9
abdessalem16 <abdessalemgitlahouel@gmail.com>
2026-08-27 04:28:18 +0200

    j'ai fait pagination et recherche

--- ecommerce/shop/views.py
+++ ecommerce/shop/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
-
 from shop.models import Product
+from django.core.paginator import Paginator
 
-# Create your views here.
 def index(request):
     product_object = Product.objects.all()
+    item_name =request.GET.get('item-name')
+    if item_name != '' and item_name is not None:
+        product_object = Product.objects.filter(title__icontains=item_name)
+    paginator = Paginator(product_object,4)
+    page = request.GET.get('page')
+    product_object = paginator.get_page(page)
     return render(request,"shop/index.html",{'product_object': product_object})
