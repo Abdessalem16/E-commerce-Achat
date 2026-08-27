0c78dfd457d857d8c261a2036fe9e5b054c2021f
abdessalem16 <abdessalemgitlahouel@gmail.com>
2026-08-27 03:39:30 +0200

    afficher les produits dans interface index

--- ecommerce/shop/views.py
+++ ecommerce/shop/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 
+from shop.models import Product
+
 # Create your views here.
 def index(request):
-    return render(request,"shop/index.html")
+    product_object = Product.objects.all()
+    return render(request,"shop/index.html",{'product_object': product_object})
